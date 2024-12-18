97114ec3932792d9d9065844f5400f57727d90b8
Bruce Lucas <bdlucas1@gmail.com>
2024-12-18 16:17:59 -0500

    Compute tolerances and gaussian width based on object size

--- dev/align.py
+++ dev/align.py
@@ -31,21 +31,24 @@ def sample_points(mesh, n):
     print(f"{len(grid)} grid points")
     #viz.show(mesh, grid)
 
-    # find the closest point on the mesh to each grid
+    # find the closest point on the mesh to each grid point
     closest = find_closest(mesh, grid)
 
     # filter the closest points to include only those near the grid point it came from
-    # check duplicates - there shouldn't be any (or very few) 
+    # sanity check: there should be no (or very few) duplicates
     points = [p for p, g in zip(closest, grid) if np.max(np.abs(p-g)) <= cell_size/2]
     print(f"{len(points)} close points; {len(set(tuple(p) for p in points))} without duplicates")
     #viz.show(mesh, points)
 
     return points, cell_size
 
+# align two meshes by moving one
+# returns the moved mesh
+# n is the number of sample points to place on the moving mesh
 def align(stationary, moving, n=5000):
 
     # place approximately n sample points on the moving_points mesh
-    moving_points, cell_size = sample_points(moving)
+    moving_points, cell_size = sample_points(moving, n)
     print(f"{len(moving_points)} sample points")
 
     # compute distance squared for each point in moving points
@@ -58,30 +61,37 @@ def align(stationary, moving, n=5000):
         sqdists = np.array([np.dot(d, d) for d in deltas])
 
         #viz.show(stationary, closest)
-        print(x)
+        print(f"delta: [{delta[0]:.3f} {delta[1]:.3f} {delta[2]:.3f}]")
 
         return sqdists
 
     # minimize total penalty for a given initial delta, tolerance, and penalty function
     # penalty is a function of the array of squared distances, e.g.
-    #     sum of squared distances
-    #     negative sum of exp of negative squared distances (i.e. gaussian)
+    #     sum of squared distances (first pass minimize)
+    #     gaussian: negative sum of exp of negative squared distances (second pass minimize)
     # "L-BFGS-B" minimization method had by far fewer goal function executions
     # which is important here because they are expensive
-    def minimize(delta, tol, penalty):
-        print(f"start {delta}, tol {tol}")
+    def minimize(delta0, tol, penalty):
+        print(f"minimize pass, tol: {tol:.1g}")
         fun = lambda x: penalty(sqdists(x))
-        result = scipy.optimize.minimize(fun, x0=delta.tolist(), method="L-BFGS-B", tol=tol)
+        result = scipy.optimize.minimize(fun, x0=delta0.tolist(), method="L-BFGS-B", tol=tol)
         return result.x
     
+    # compute size-based algorithm parameters
+    xmin, xmax, ymin, ymax, zmin, zmax = stationary.bounds
+    size = np.sqrt((xmax-xmin)**2 + (ymax-ymin)**2 + (zmax-zmin)**2)
+    tol1 = size * 1e-4   # first pass tolerance
+    tol2 = size * 1e-6   # second pass tolerance
+    width2 = size / 100  # second pass width (inverse sharpness) of gaussian
+
     # initial guess is to align centroids
     delta = np.average(stationary.points, axis=0) - np.average(moving.points, axis=0)
 
     # minimize penalty using sum of squared distances
-    delta = minimize(delta, 1e-2, penalty = lambda sqdists: sum(sqdists))
+    delta = minimize(delta, tol = tol1, penalty = lambda sqdists: sum(sqdists))
 
     # now minimize further using a sharper penalty function that ignores points that aren't close
-    delta = minimize(delta, 1e-5, penalty = lambda sqdists: -sum(np.exp(-sqdists/cell_size**2))) # TODO: scale?
+    delta = minimize(delta, tol = tol2, penalty = lambda sqdists: -sum(np.exp(-sqdists/width2)))
 
     # move it and return
     return moving.translate(delta)
